1b77fc667668dbb0d13bcb673cf45b2a6face8e8
Ryan Z <ryanzade@hotmail.com>
2022-12-20 17:52:23 +1030

    models index.js first commit

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // through: {
+  //   model: Trip,
+  //   unique: false
+  // },
+  // as: 'planned_trips'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
 
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tags'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags_products'
+});
 
 module.exports = {
   Product,
