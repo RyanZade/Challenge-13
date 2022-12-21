9104844de2eac9d8c79dcb2602e0414ee7c1c2a6
Ryan Z <ryanzade@hotmail.com>
2022-12-21 15:11:50 +1030

    update routes/api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,74 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagsData = await Tag.findAll({
+      include: [{ model: Product,
+        as: 'tags_products' }]
+      });
+    res.status(200).json(tagsData);
+  } catch (error) {
+    
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagsData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product,
+        through: ProductTag,
+        as: 'tags_products' }]
+    });
+
+    if (!tagsData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagsData = await Tag.create(req.body);
+    res.status(200).json(tagsData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagsData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagsData) {
+      res.status(404).json({ message: 'No tags found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
